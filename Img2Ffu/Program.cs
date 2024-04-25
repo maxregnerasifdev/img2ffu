6e3b9a8253e13b92921c86a96cb1ff889bc71ea7
maxregnerklos <135330858+maxregnerklos@users.noreply.github.com>
2024-04-25 10:59:08 +0200

    Update Program.cs

--- Img2Ffu/Program.cs
+++ Img2Ffu/Program.cs
@@ -1,17 +1,13 @@
-﻿/*
-
-Copyright (c) 2019, Gustave Monce - gus33000.me - @gus33000
-
+/*
+Copyright (c) 2019-2024, Gustave Monce - gus33000.me - @gus33000
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
-
 The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
-
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -19,14 +15,9 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
-
 */
 using CommandLine;
 using DiscUtils;
-using Img2Ffu.Writer;
-using Img2Ffu.Writer.Data;
-using Img2Ffu.Writer.Flashing;
-using Img2Ffu.Writer.Manifest;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -41,31 +32,29 @@ namespace Img2Ffu
     {
         private static void Main(string[] args)
         {
-            _ = Parser.Default.ParseArguments<Options>(args).WithParsed(o =>
+            Parser.Default.ParseArguments<Options>(args).WithParsed(o =>
             {
                 Logging.Log("img2ffu - Converts raw image (img) files into full flash update (FFU) files");
                 Logging.Log("Copyright (c) 2019-2024, Gustave Monce - gus33000.me - @gus33000");
-                Logging.Log("Copyright (c) 2018, Rene Lergner - wpinternals.net - @Heathcliff74xda");
-                Logging.Log("Released under the MIT license at github.com/WOA-Project/img2ffu");
-                Logging.Log("");
 
                 try
                 {
-                    string ExcludedPartitionNamesFilePath = o.ExcludedPartitionNamesFilePath;
+                    string excludedPartitionNamesFilePath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), o.ExcludedPartitionNamesFilePath);
 
-                    if (!File.Exists(ExcludedPartitionNamesFilePath))
+                    if (!File.Exists(excludedPartitionNamesFilePath))
                     {
-                        ExcludedPartitionNamesFilePath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), o.ExcludedPartitionNamesFilePath);
-                    }
-
-                    if (!File.Exists(ExcludedPartitionNamesFilePath))
-                    {
-                        Logging.Log("Something happened.", Logging.LoggingLevel.Error);
-                        Logging.Log("We couldn't find the provisioning partition file.", Logging.LoggingLevel.Error);
-                        Logging.Log("Please specify one using the corresponding argument switch", Logging.LoggingLevel.Error);
+                        Logging.Log("Error: Could not find the provisioning partition file.", Logging.LoggingLevel.Error);
                         Environment.Exit(1);
                         return;
                     }
+
+                    GenerateFFU(o.InputFile, o.FFUFile, o.PlatformID.Split(';'), o.SectorSize, o.BlockSize, o.AntiTheftVersion, o.OperatingSystemVersion, File.ReadAllLines(excludedPartitionNamesFilePath), o.MaximumNumberOfBlankBlocksAllowed, FlashUpdateVersion.V2, new List<DeviceTargetInfo>());
+                }
+                catch (Exception ex)
+                {
+                    Logging.Log($"An exception occurred: {ex.Message}", Logging.LoggingLevel.Error);
+                    Environment.Exit(1);
+                }
 
                     GenerateFFU(o.InputFile, o.FFUFile, o.PlatformID.Split(';'), o.SectorSize, o.BlockSize, o.AntiTheftVersion, o.OperatingSystemVersion, File.ReadAllLines(ExcludedPartitionNamesFilePath), o.MaximumNumberOfBlankBlocksAllowed, FlashUpdateVersion.V2, []);;
                 }
